bb23f0a843dffbf78748960af07a61f14247f000
minggege-cpu <54826434+minggege-cpu@users.noreply.github.com>
2020-05-09 10:51:33 +0000

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,11 +6,14 @@ def bubble_sort(nums):
     比较相邻的元素，如果前面的比后面的大，就交换他们
     时间复杂度：T(n)=O(n^2)
     '''
-
     for i in range(len(nums)-1):      #冒泡排序一共要运行的次数
+        count = 0
         for j in range(len(nums)-i-1):   #j是下标 每循环一次还未排好序的最大值都会找到正确的位置
             if nums[j] > nums[j+1]:
                 nums[j], nums[j+1] = nums[j+1], nums[j]
+                count += 1
+        if count == 0:  # 优化 循环了N次后发现没有变化即退出循环
+            break
     return nums
 
 
